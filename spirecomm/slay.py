48a69536d7eb81532c69909d589c71e73210172d
Justin Mucelli <10000turtles@gmail.com>
2023-03-22 20:36:54 -0400

    add

---
+++ spirecomm/slay.py
@@ -0,0 +1,22 @@
+import itertools
+import datetime
+import sys
+
+from spirecomm.communication.coordinator import Coordinator
+from spirecomm.ai.agent import SimpleAgent
+from spirecomm.spire.character import PlayerClass
+
+if __name__ == "__main__":
+        
+    agent = SimpleAgent()
+    coordinator = Coordinator()
+
+    coordinator.signal_ready()
+    coordinator.register_command_error_callback(agent.handle_error)
+    coordinator.register_state_change_callback(agent.get_next_action_in_game)
+    coordinator.register_out_of_game_callback(agent.get_next_action_out_of_game)
+
+    result = coordinator.play_one_game(PlayerClass.THE_SILENT)
+
+
+ 
