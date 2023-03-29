0ad5e8e154d29bfd840f1777ae10a38587f49643
Justin Mucelli <10000turtles@gmail.com>
2023-03-29 18:56:02 -0400

    Codin Codin Codin

--- spirecomm/slay.py
+++ spirecomm/slay.py
@@ -10,25 +10,25 @@ from spirecomm.spire.character import PlayerClass
 from spirecomm.spire.game import Game
 
 if __name__ == "__main__":
-        
-    agent = CoolRadicalAgent()
-    coordinator = Coordinator()
 
-    coordinator.signal_ready()
-    coordinator.register_command_error_callback(agent.handle_error)
-    coordinator.register_state_change_callback(agent.get_next_action_in_game)
-    coordinator.register_out_of_game_callback(agent.get_next_action_out_of_game)
+  agent = CoolRadicalAgent()
+  coordinator = Coordinator()
 
-    # f = open('jawWorm1hp.json')
-    # f = open('jaw_worm_strength.json')
-    f = open('jawWorm.json')
-    communication_state = json.load(f)
+  coordinator.signal_ready()
+  coordinator.register_command_error_callback(agent.handle_error)
+  coordinator.register_state_change_callback(agent.get_next_action_in_game)
+  coordinator.register_out_of_game_callback(agent.get_next_action_out_of_game)
 
-    agent.get_next_action_in_game(Game.from_json(communication_state.get("game_state"), communication_state.get("available_commands")))
+#   f = open('jawWorm1hp.json')
+  # f = open('jaw_worm_strength.json')
+  f = open('jawWorm.json')
+  communication_state = json.load(f)
+
+  agent.get_next_action_in_game(Game.from_json(communication_state.get("game_state"), communication_state.get("available_commands")))
 
 
 # if __name__ == "__main__":
-        
+
 #     agent = SimpleAgent()
 #     coordinator = Coordinator()
 
@@ -38,7 +38,3 @@ if __name__ == "__main__":
 #     coordinator.register_out_of_game_callback(agent.get_next_action_out_of_game)
 
 #     result = coordinator.play_one_game(PlayerClass.IRONCLAD)
-   
-
-
- 
